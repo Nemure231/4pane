9a5895a6b6dc8179275b24bcc7ea066833fa04a1
Nemure231 <neruhi5321@gmail.com>
2022-05-17 22:23:12 +0700

    fix loading

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -831,7 +831,7 @@ export default {
                   v-text="pickPane.paneName"></span>
 
                   <div class="absolute bg-cover bg-center inset-0 top-2 flex justify-center items-center">
-                     <svg 
+                     <svg :class="isLoading"
                        class="animate-spin h-12 w-12 text-[#2155cd]" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24">
                         <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4"></circle>
                         <path class="opacity-75" fill="currentColor" d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"></path>
